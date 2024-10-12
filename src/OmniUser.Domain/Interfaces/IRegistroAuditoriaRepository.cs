b4697358c67e5e975350da33e357192b2a23a724
Brenno Martins <brenno.martins@jointecnologia.com.br>
2024-10-12 15:21:42 -0300

    ✨ feature: remove os registros de auditoria

--- src/OmniUser.Domain/Interfaces/IRegistroAuditoriaRepository.cs
+++
@@ -1,9 +0,0 @@
-using OmniUser.Domain.Models;
-
-namespace OmniUser.Domain.Interfaces;
-
-public interface IRegistroAuditoriaRepository : IDisposable
-{
-    Task<RegistroAuditoria?> Obter(int id);
-    Task<IEnumerable<RegistroAuditoria?>> ObterTodos();
-}
--- src/OmniUser.Infrastructure/Context/OmniUserDbContext.cs
+++ src/OmniUser.Infrastructure/Context/OmniUserDbContext.cs
@@ -1,6 +1,4 @@
-using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Configuration;
 using OmniUser.Domain.Models;
 
@@ -21,7 +19,6 @@ public class OmniUserDbContext : DbContext
     }
 
     public DbSet<Usuario> Usuarios { get; init; } = default!;
-    public DbSet<RegistroAuditoria> RegistrosAuditoria { get; init; } = default!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -31,106 +28,5 @@ public class OmniUserDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(OmniUserDbContext).Assembly);
-    }
-
-    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        var agora = DateTime.Now;
-        var entityEntries = ChangeTracker.Entries().ToList();
-
-        foreach (var entry in entityEntries)
-        {
-            if (entry.Entity is EntidadeRastreada entidadeRastreada)
-            {
-                switch (entry.State)
-                {
-                    case EntityState.Added:
-                        entidadeRastreada.DefinirDataDeCriacao(agora);
-                        entidadeRastreada.DefinirDataDeAtualizacao(agora);
-                        break;
-                    case EntityState.Modified:
-                        entidadeRastreada.DefinirDataDeAtualizacao(agora);
-                        break;
-
-                    case EntityState.Detached:
-                    case EntityState.Unchanged:
-                    case EntityState.Deleted:
-                    default:
-                        break;
-                }
-            }
-
-            // ReSharper disable once InvertIf
-            if (entry.State is EntityState.Added or EntityState.Modified or EntityState.Deleted)
-            {
-                var registroAuditoria = new RegistroAuditoria
-                {
-                    Entidade = entry.Entity.GetType().Name,
-                    Acao = entry.State.ToString(),
-                    Timestamp = agora,
-                    Alteracoes = ObterMudancas(entry)
-                };
-
-                RegistrosAuditoria.Add(registroAuditoria);
-            }
-        }
-
-        return await base.SaveChangesAsync(cancellationToken);
-
-        // foreach (var entry in entityEntries.Where(entry => entry.Entity.GetType().GetProperty("CriadoEm") != null))
-        // {
-        //     if (entry.State == EntityState.Added)
-        //     {
-        //         entry.Property("CriadoEm").CurrentValue = DateTime.Now;
-        //     }
-        // }
-        //
-        // foreach (var registroAuditoria in entityEntries
-        //              .Where(e => e.State is EntityState.Added or EntityState.Modified or EntityState.Deleted)
-        //              .Select(entidadeModificada => new RegistroAuditoria
-        //              {
-        //                  Entidade = entidadeModificada.Entity.GetType().Name,
-        //                  Acao = entidadeModificada.State.ToString(),
-        //                  Timestamp = DateTime.Now,
-        //                  Alteracoes = GetChanges(entidadeModificada)
-        //              }))
-        // {
-        //     RegistrosAuditoria.Add(registroAuditoria);
-        // }
-        //
-        // return base.SaveChangesAsync(cancellationToken);
-    }
-
-    private static string ObterMudancas(EntityEntry entidade)
-    {
-        var alteracoes = new Dictionary<string, object>();
-
-        foreach (var propriedade in entidade.OriginalValues.Properties)
-        {
-            var original = entidade.OriginalValues[propriedade];
-            var atual = entidade.CurrentValues[propriedade];
-
-            if (entidade.State is EntityState.Added)
-            {
-                alteracoes[propriedade.Name] = atual ?? string.Empty;
-            }
-            else if (entidade.State is EntityState.Deleted)
-            {
-                alteracoes[propriedade.Name] = original ?? string.Empty;
-            }
-            else if (!Equals(original, atual))
-            {
-                alteracoes[propriedade.Name] = new { De = original, Para = atual };
-            }
-        }
-
-        var id = entidade.Metadata.FindPrimaryKey()?.Properties.FirstOrDefault();
-        if (id is not null)
-        {
-            alteracoes["Id"] = entidade.OriginalValues.GetValue<int>("Id");
-        }
-
-        var retorno = JsonSerializer.Serialize(alteracoes);
-        return retorno;
     }
 }
